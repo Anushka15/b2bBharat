3af808e12cfb23ed7318596dbdc500dee464c704
Anushka Kulkarni <anushkakulkarni15@gmail.com>
2020-07-15 19:40:01 +0530

    login done

--- src/main/java/com/b2b/controller/LoginController.java
+++ src/main/java/com/b2b/controller/LoginController.java
@@ -2,17 +2,18 @@ package com.b2b.controller;
 
 import javax.validation.Valid;
 
+import com.b2b.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.b2b.model.User;
-import com.b2b.service.UserService;
 
 @Controller
 public class LoginController {
@@ -20,64 +21,61 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = {"/","/index"})
+    @RequestMapping(value = {"/","/index"}, method=RequestMethod.GET)
     public ModelAndView home() {
         ModelAndView model = new ModelAndView();
         model.setViewName("index");
         return model;
     }
 
+    @RequestMapping(value = {"/login","/index/login"}, method=RequestMethod.GET)
     public ModelAndView login() {
         ModelAndView model = new ModelAndView();
         model.setViewName("login");
         return model;
     }
 
-    /*@RequestMapping(value= {"/signup"}, method=RequestMethod.GET)
-    public ModelAndView signup() {
-        ModelAndView model = new ModelAndView();
+    @RequestMapping(value = {"/registration","/index/registration"}, method=RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
         User user = new User();
-        model.addObject("user", user);
-        model.setViewName("user/signup");
-
-        return model;
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
     }
 
-    @RequestMapping(value= {"/signup"}, method=RequestMethod.POST)
+
+    @RequestMapping(value= {"/registration","/index/registration"}, method=RequestMethod.POST)
     public ModelAndView createUser(@Valid User user, BindingResult bindingResult) {
         ModelAndView model = new ModelAndView();
         User userExists = userService.findUserByEmail(user.getEmail());
 
         if(userExists != null) {
-            bindingResult.rejectValue("email", "error.user", "This email already exists!");
+            bindingResult.rejectValue("email", "error.user", "This email already exists! Go to the login page");
         }
         if(bindingResult.hasErrors()) {
-            model.setViewName("user/signup");
+            model.setViewName("registration");
         } else {
             userService.saveUser(user);
             model.addObject("msg", "User has been registered successfully!");
             model.addObject("user", new User());
-            model.setViewName("user/signup");
+            model.setViewName("/loginAgain");
         }
 
         return model;
     }
 
-    @RequestMapping(value= {"/home/home"}, method=RequestMethod.GET)
-    public ModelAndView home() {
-        ModelAndView model = new ModelAndView();
+    @GetMapping(value="/home")
+    public ModelAndView loginDone(){
+        ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByEmail(auth.getName());
-
-        model.addObject("userName", user.getFirstname() + " " + user.getLastname());
-        model.setViewName("home/home");
-        return model;
+        modelAndView.addObject("name", "Welcome " + user.getName());
+        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("/home");
+        return modelAndView;
     }
 
-    @RequestMapping(value= {"/access_denied"}, method=RequestMethod.GET)
-    public ModelAndView accessDenied() {
-        ModelAndView model = new ModelAndView();
-        model.setViewName("errors/access_denied");
-        return model;
-    }*/
+
+
 }
